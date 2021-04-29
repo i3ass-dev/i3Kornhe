8f59e9671020edf8903108faee1ee9be422182ea
bud <robstenklippa@gmail.com>
2021-04-29 23:21:45 +0200

    [ADD] cleanup file and function to lib

---
+++ lib/cleanup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cleanup() {
+
+  ((__o[verbose])) && ERM "f ${FUNCNAME[0]}()"
+
+  local qflag k s
+
+  ((__o[verbose])) || qflag='-q'
+
+  ((${#_v[@]})) && varset
+
+  # [[ -n $_msgstring ]] && i3-msg "${qflag:-}" "$_msgstring"
+  
+  ((${#_r[@]})) && {
+    for k in "${!_r[@]}"; do s+="[$k] ${_r[$k]};" ; done
+    i3-msg "${qflag:-}" "$s"
+  }
+
+  ((__o[verbose])) && {
+    local delta=$(( ($(date +%s%N)-_stamp) /1000 ))
+    local time=$(((delta / 1000) % 1000))
+    ERM  "---i3fyra done: ${time}ms---"
+  }
+}
